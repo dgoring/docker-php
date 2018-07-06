983cd98b4149b672a59e83c0f18e7a57a7a1b6ba
David Goring <david.lj.goring@gmail.com>
2018-07-06 09:50:53 +0100

    Fix Cron

--- 7.0/nginx/run.sh
+++ 7.0/nginx/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # if commands are entered for crontab - check it
-if [ -f /usr/local/config/crontab ]; then
-  crontab -u www-data /usr/local/config/crontab
+if [ -f /config/crontab ]; then
+  crontab -u www-data /config/crontab
 fi
 
 exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
