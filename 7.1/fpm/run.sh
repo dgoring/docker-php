ca78725dbfccffdd450e8a182bdf7ea36d13e8d0
David Goring <david.lj.goring@gmail.com>
2020-03-13 06:57:25 +0000

    Fix PHP ini includes

--- 7.1/fpm/run.sh
+++ 7.1/fpm/run.sh
@@ -5,7 +5,7 @@ if [ -f /config/crontab ]; then
   crontab /config/crontab
 fi
 
-cat /etc/php.ini   > /usr/local/etc/php/php.ini
-cat /config/*.ini >> /usr/local/etc/php/php.ini
+sudo bash -c 'cat /etc/php.ini   > /usr/local/etc/php/php.ini'
+sudo bash -c 'cat /config/*.ini >> /usr/local/etc/php/php.ini'
 
 exec sudo supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
